ddc98dc605174c24762ea4d8e79eb01e6497186b
Georgy <kotlyar@cadmean.ru>
2020-12-19 15:21:06 +0300

    Update provider.py

--- provider.py
+++ provider.py
@@ -1,12 +1,6 @@
 import json
-from pip._vendor import requests
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-import csv
-chromedriver = 'chromedriver.exe'
-options = webdriver.ChromeOptions()
-options.add_argument('headless')  # для открытия headless-браузера
-browser = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
 
 
 def parse_provider_links():
@@ -14,7 +8,7 @@ def parse_provider_links():
     soup = BeautifulSoup(req.text, 'lxml')
     url = []
     for a in soup.find_all('a', href=True):
-        if 'reestrNumber' in a['href'] and 'journal-events' not in a['href']:
+        if 'reestrNumber' in a['href'] and 'journal-events' not in a['href'] and a['href'] not in url:
             url.append(a['href'])
             print(a['href'])
     url_starter = 'https://zakupki.gov.ru'
@@ -36,8 +30,9 @@ def parse_provider_links():
             json1block[content_title_list[i]] = content_info_list[i]
         jsonfile['list'].append(json1block)
         if not (jsonfile == {} or jsonfile == ''):
-            with open('C:\\Users\\Viktor\\Desktop\\' + 'hello' + '.json', 'w', encoding='utf-8') as write_file:
+            with open('C:\\Users\\Georgy\\Desktop\\' + 'hello' + '.json', 'w', encoding='utf-8') as write_file:
                 json.dump(jsonfile, write_file, indent=4, ensure_ascii=False)
         print('---------------------------------------------------------------')
+
 
 parse_provider_links()
