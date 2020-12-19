69a6384a2b7ca30becf045482209a935eb5cb25f
Georgy <kotlyar@cadmean.ru>
2020-12-19 14:53:19 +0300

    more

--- provider.py
+++ provider.py
@@ -1,23 +1,37 @@
 import json
-import requests
-import regex as re
-import pandas as pd
+from pip._vendor import requests
 from bs4 import BeautifulSoup
 
 
-def download_timetable():
-    global error_counter
+def parse_provider_links():
     req = requests.get('https://zakupki.gov.ru/epz/eruz/search/results.html')
     soup = BeautifulSoup(req.text, 'lxml')
     url = []
-    for a in soup.find_all('a', class_='uk-link-toggle', href=True):
-        if 'pdf' not in a['href'] and 'xlsx' in a['href'] or 'XLSX' in a['href']:
+    for a in soup.find_all('a', href=True):
+        if 'reestrNumber' in a['href'] and 'journal-events' not in a['href']:
             url.append(a['href'])
             print(a['href'])
+    url_starter = 'https://zakupki.gov.ru'
+    content_title_list = []
+    content_info_list = []
+    jsonfile = {'list': []}
+    for num, url_x in enumerate(url, 0):
+        provider_data = requests.get(url_starter+url_x, allow_redirects=True)
+        soup2 = BeautifulSoup(provider_data.text, 'lxml')
+        containers = soup2.find_all("section")
+        for j in containers:
+            content_title = j.find(class_='section__title')
+            content_info = j.find(class_='section__info')
+            if (content_title and content_info) is not None:
+                content_title_list.append(content_title.text.strip())
+                content_info_list.append(content_info.text.strip())
+        json1block = {}
+        for i in range(len(content_info_list)):
+            json1block[content_title_list[i]] = content_info_list[i]
+        jsonfile['list'].append(json1block)
+        if not (jsonfile == {} or jsonfile == ''):
+            with open('C:\\Users\\Viktor\\Desktop\\' + 'hello' + '.json', 'w', encoding='utf-8') as write_file:
+                json.dump(jsonfile, write_file, indent=4, ensure_ascii=False)
+        print('---------------------------------------------------------------')
 
-    for url_x in url:
-        r = requests.get(url_x, allow_redirects=True)
-        open(url_x.rsplit('/', 1)[1], 'wb').write(r.content)
-        print(url_x.rsplit('/', 1)[1])
-
-    print(error_counter)
+parse_provider_links()
